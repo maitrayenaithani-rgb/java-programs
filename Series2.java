1886109603fb39e68093c43807f464fc53a35ff1
maitrayenaithani-rgb <maitrayenaithani@gmail.com>
2025-10-21 22:51:27 +0530

    Create Series2.java

---
+++ Series2.java
@@ -0,0 +1,10 @@
+public class Series2 {
+    public static void main(String[] args) {
+        int terms = 8;
+
+        for (int i = 1; i <= terms; i++) {
+            int v= i * i + 1;
+            System.out.print(v + " ");
+        }
+    }
+}
