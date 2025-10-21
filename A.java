8f39948f90a54a73161af065510eda86035a6aa6
maitrayenaithani-rgb <maitrayenaithani@gmail.com>
2025-10-21 22:50:36 +0530

    Create A.java

---
+++ A.java
@@ -0,0 +1,12 @@
+public class A {
+    public static void main(String[] args) {
+int n=1;
+int d=2;
+System.out.println(n+" ");
+        for (int i = 2; i <= 8; i++) {
+             n=n+d;
+           System.out.print(n + " ");
+d=d+4;
+        }
+    }
+}
