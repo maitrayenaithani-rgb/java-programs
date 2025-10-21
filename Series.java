93c24ab5371999795d3abb29d5d555087d6a46a9
maitrayenaithani-rgb <maitrayenaithani@gmail.com>
2025-10-21 22:50:59 +0530

    Create Series.java

---
+++ Series.java
@@ -0,0 +1,10 @@
+public class Series {
+    public static void main(String[] args) {
+        int n = 1;
+
+        for (int i = 1; i <= 8; i++) {
+            System.out.print(n + " ");
+            n = n * 10 + 1;
+        }
+    }
+}
