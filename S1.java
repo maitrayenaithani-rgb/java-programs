0c215690451f7880021beb4b280bd85894be99b0
maitrayenaithani-rgb <maitrayenaithani@gmail.com>
2025-10-22 17:55:11 +0530

    Create S1.java

---
+++ S1.java
@@ -0,0 +1,11 @@
+public class S1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 8; i++) {
+            int term = (i * i*i) + 1;
+            System.out.print(term);
+            if (i < 8) {
+                System.out.print(", ");
+            }
+        }
+    }
+}
