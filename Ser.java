589196bda2a545560bfc7a5452fd8957096b79f5
maitrayenaithani-rgb <maitrayenaithani@gmail.com>
2025-10-23 17:10:44 +0530

    Create Ser.java

---
+++ Ser.java
@@ -0,0 +1,10 @@
+public class Ser{
+public static void main(String[] args) {
+for(int i=1;i<=8;i++){
+int term=(i*i*i)-1;
+System.out.print(term);
+if(i<8)
+System.out.print(" , ");
+          }
+      }
+   }
