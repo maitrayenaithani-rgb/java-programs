2ceafe742cc6d6c20f25a83a2c471545a2e8e87b
maitrayenaithani-rgb <maitrayenaithani@gmail.com>
2025-10-31 22:41:06 +0530

    Create Number.java

---
+++ Number.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Numbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n");
+        int n = sc.nextInt();
+        for(int i = n; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
