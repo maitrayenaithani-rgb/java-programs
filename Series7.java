6bd24eaef29d9a65eec83efb1ce6c3d6dd0f61c6
maitrayenaithani-rgb <maitrayenaithani@gmail.com>
2025-11-01 12:17:24 +0530

    Create Series7.java

---
+++ Series7.java
@@ -0,0 +1,9 @@
+public class Series
+ {
+    public static void main(String[] args) 
+{
+Scanner sc=new Scanner(System.in);
+        for (int i = 1; i <= 6; i++)
+            System.out.print(i * (i + 1) + " ");
+    }
+}
