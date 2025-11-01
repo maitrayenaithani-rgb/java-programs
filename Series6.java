58f1a9fce98d99f34af1a9b40b0fad1850a0acef
maitrayenaithani-rgb <maitrayenaithani@gmail.com>
2025-11-01 12:14:43 +0530

    Create Series6.java

---
+++ Series6.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class FractionSeries {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double sum = 0;
+        for (int i = 1; i <= n; i++){
+        sum += 1.0 / i;
+        System.out.println(sum);
+    }
+  }
+}
