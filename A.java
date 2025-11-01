bcfa484d1e7e3015fd2e67a97c51856fb4eee745
maitrayenaithani-rgb <maitrayenaithani@gmail.com>
2025-11-01 20:30:19 +0530

    Update A.java

--- A.java
+++ A.java
@@ -2,7 +2,7 @@ public class A {
     public static void main(String[] args) {
 int n=1;
 int d=2;
-System.out.println(n+" ");
+System.out.print(n+" ");
         for (int i = 2; i <= 8; i++) {
              n=n+d;
            System.out.print(n + " ");
