95d2c4cdce9a0bfc26bfcc1b1adfc50ca00aa473
maitrayenaithani-rgb <maitrayenaithani@gmail.com>
2025-11-02 14:17:44 +0530

    Create Countvowel.java

---
+++ Countvowel.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class CtVowels {
+ public static void main(String[] args) {   Scanner sc=new Scanner(System.in);
+ System.out.println("Enter a sentence");
+ String str=sc.nextLine();
+        int count = 0;
+        str = str.toLowerCase();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                count++;
+        }
+
+  System.out.println("Number of vowels " + count);
+    }
+}
