99294e212c3d62adc60c519a9fd256d7776c696e
maitrayenaithani-rgb <maitrayenaithani@gmail.com>
2025-11-02 15:43:10 +0530

    Create Concat.java

---
+++ Concat.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Concat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first string");
+        String str1 = sc.nextLine();
+        System.out.println("Enter the second string");
+        String str2 = sc.nextLine();
+        String r= str1 + str2; 
+        System.out.println("Concatenated string " + r);
+    }
+}
