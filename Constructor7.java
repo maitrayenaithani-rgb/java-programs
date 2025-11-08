5999729baf3f657f0ef795a304864abee8c1e28d
maitrayenaithani-rgb <maitrayenaithani@gmail.com>
2025-11-08 19:11:25 +0530

    Update Constructor7.java

--- Constructor7.java
+++ Constructor7.java
@@ -7,7 +7,7 @@ class Fact {
         }
         System.out.println("Factorial of " + num + " is: " + f);
     }
-}
+
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number to find factorial");
