7ebb00b4d39772b8fabcc882b40b197283d07b9f
maitrayenaithani-rgb <maitrayenaithani@gmail.com>
2025-11-08 18:44:09 +0530

    Create Constructor7.java

---
+++ Constructor7.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Fact {
+    Fact(int num) {
+        int f = 1;
+        for (int i = 1; i <= num; i++) {
+            f = f*i;
+        }
+        System.out.println("Factorial of " + num + " is: " + f);
+    }
+}
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to find factorial");
+        int n = sc.nextInt();
+        Fact f = new Fact(n);
+    }
+}
